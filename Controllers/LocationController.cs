c840c1489eb3e7b3be671af4f5362cf206414590
Marco Littel <ml@newspark.nl>
2022-04-23 12:10:36 +0200

    First commit

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace LocationService
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+
+        private static readonly HttpClient client = new HttpClient();
+        private readonly ILogger<LocationController> _logger;
+        private readonly IConfiguration _config;
+
+        public LocationController(ILogger<LocationController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpGet]
+        public IEnumerable<Location> Get()
+        {
+            return Enumerable.Range(1, 1).Select(index => new Location
+            {
+                longitude = "52.07963511282511",
+                latitude= ", 4.86157988871688"
+            }
+                ).ToArray();
+            
+        }
+
+        [HttpPost]
+        public async Task<IEnumerable<Location>> PostAsync([FromBody] InputAddress address)
+        {
+            var key = _config["OpenCageKey"];
+            var data = HttpUtility.UrlEncode(address.address);
+            var url = "https://api.opencagedata.com/geocode/v1/json?key="+ key + "&pretty=1&q=" + data;
+
+            var response =  await client.GetStringAsync(url);
+            var res = JsonConvert.DeserializeObject<JObject>(response);
+            
+            var resList = res["results"];
+            
+            var lat = resList[0]["bounds"]["northeast"]["lat"].Value<string>();
+            var lng = resList[0]["bounds"]["northeast"]["lng"].Value<string>();
+
+
+            return Enumerable.Range(1, 1).Select(index => new Location
+            {
+                longitude = lng,
+                latitude = lat
+            }
+                ).ToArray();
+        }
+    }
+}
---
+++ Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationService
+{
+    public class Location
+    {
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+    }
+
+    public class InputAddress
+    {
+        public string address { get; set; }
+    }
+}
